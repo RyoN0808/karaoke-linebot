5ca6a10894721db70c886a465e73e378ef6e42d5
Ryo N <ryo@RyonoMacBook-Air.local>
2025-07-11 11:42:11 +0900

    change id

--- utils/richmenu.py
+++ utils/richmenu.py
@@ -1,4 +1,6 @@
+import os
 from linebot.v3.messaging import MessagingApi
+from linebot.v3.messaging.configuration import Configuration
 
 # 使用するリッチメニューID
 RICH_MENU_ID = "richmenu-81d23e94b05ddc9b1494a4d1e9c412f4"
@@ -7,7 +9,9 @@ def create_and_link_rich_menu(user_id: str):
     """
     既存のリッチメニューIDをLINEユーザーに紐付ける
     """
-    messaging_api = MessagingApi()  # MessagingApiインスタンス生成
+    access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+    configuration = Configuration(access_token=access_token)
+    messaging_api = MessagingApi(configuration)  # 明示的にConfigurationを渡す
 
     # ユーザーにリッチメニューを紐付け
     messaging_api.link_rich_menu_to_user(
